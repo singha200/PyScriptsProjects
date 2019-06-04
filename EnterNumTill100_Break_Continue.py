540ddc3a5e0a95e7de904717a22c6ad6e85eaa21
CodeNameTurtle <singhashwani1989@gmail.com>
2019-06-05 04:10:20 +0530

    Include Doctring

--- EnterNumTill100_Break_Continue.py
+++ EnterNumTill100_Break_Continue.py
@@ -1,10 +1,13 @@
-#Loop will continue till the number greater than 100 is entered
+def number1():
+    """Loop will continue till the number greater than 100 is entered"""  #Docstring Gives summary of the function
+    while(True):
+        a = int(input("Enter a number"))
+        if a < 100:
+            print("Try again")
+            continue #Loop will continue till the if statement is true
+        else:
+            print("Congratulation you enter number greater than 100")
+            break #Loop will break
 
-while(True):
-    a = int(input("Enter a number"))
-    if a < 100:
-        print("Try again")
-        continue #Loop will continue till the if statement is true
-    else:
-        print("Congratulation you enter number greater than 100")
-        break #Loop will break
+number1()
+print(number1.__doc__) #Printing whatever written in the DocString of the function
