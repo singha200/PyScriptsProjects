abfb0ee604e6ab9afb148a7ef0f4560fa01d6ecb
CodeNameTurtle <singhashwani1989@gmail.com>
2019-07-14 08:34:14 +0530

    Python Request library demo for fetching plan prices from Github

--- pyrequestsDemo.py
+++ pyrequestsDemo.py
@@ -17,7 +17,7 @@ print("Response text :", r.json())
 page = requests.get('https://github.com/pricing/') #Receiving whole page in variable page
 tree = lxml.html.fromstring(page.content) #formatting the whole page from string format to html 
 print("Page Object:", tree) 
-plans = tree.xpath('//h3[@class="h3-mktg pt-2"]/text()') #using xpath to look into default class for plans on github
-pricing = tree.xpath('//h3[@class="h000-mktg lh-condensed-ultra my-2"]/text()') #Using xpath to fetch default pricing from github
+plans = tree.xpath('//h3[@class="h3-mktg pt-2"]/text()') #using tree.xpath to look into default class for plans on github
+pricing = tree.xpath('//h3[@class="h000-mktg lh-condensed-ultra my-2"]/text()') #Using tree.xpath to fetch default pricing from github
 print(f"Plans offered by github : {plans} \nPricing according to plan : {pricing}")
 
