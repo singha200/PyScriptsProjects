ef635401a202b426ccbe61aece4679690284dfdb
CodeNameTurtle <singhashwani1989@gmail.com>
2019-07-14 08:28:34 +0530

    Mapfunction Demo in python

--- Map_demo.py
+++ Map_demo.py
@@ -6,8 +6,16 @@ print(type(numbers))
 numbers = list(map(int,numbers))
 print(type(numbers))
 
+def square(a):
+    return a*a
+def cube(a):
+    return a*a*a
+func = [square, cube]
+num = [2, 3, 5, 6, 7, 8, 9]
+for i in range(5):
+    val = list(map(lambda x: x(i), func))
+    print(val)
 
-square = list(map(lambda x: x*x,numbers))
-print(square)
+
 if __name__ == "__main__":
     pass
