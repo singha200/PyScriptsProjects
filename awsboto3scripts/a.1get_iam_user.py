868c9289f3207beadf9163da2cc585edae2123f1
singha200 <23635692+singha200@users.noreply.github.com>
2022-07-21 01:03:11 +0530

    boto3 client

--- awsboto3scripts/a.1get_iam_user.py
+++ awsboto3scripts/a.1get_iam_user.py
@@ -4,3 +4,9 @@ aws_mag_con = boto3.session.Session(profile_name="cloud_user")
 iam_con = aws_mag_con.resource('iam')
 for each_user in iam_con.users.all():
     print(each_user.name)
+
+iam_con_cli = aws_mag_con.client(service_name="iam", region_name="us-east-1")
+response = iam_con_cli.list_users()
+for each_item in response['Users']:
+    print(each_item['UserName'])
+
